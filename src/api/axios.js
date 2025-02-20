508fb734e7d649484d179f9ae5698f875a0f06f1
sanoferniswan <sanoferniswan786@gmail.com>
2025-02-20 11:47:56 +0530

    dashboard changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:'https://interngo-1.onrender.com'
+    baseURL:'https://interngo.onrender.com'
 }) 
  
 // https://interngo.onrender.com 
 // http://192.168.0.141:8080 
 // http://localhost:8080 
-// 
+// https://interngo-1.onrender.com
