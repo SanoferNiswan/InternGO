eff65f9204bb13ed435468c87940adaef9aae76d
sanoferniswan <sanoferniswan786@gmail.com>
2025-02-21 10:45:27 +0530

    Im back

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:'https://interngo-1.onrender.com'
+    baseURL:'https://interngo.onrender.com'
 }) 
  
 // https://interngo.onrender.com 
