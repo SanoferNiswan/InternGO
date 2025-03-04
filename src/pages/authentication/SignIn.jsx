eeb0e70d1911b0d8461c4484909d6a07b7fc6f04
sanoferniswan <sanoferniswan786@gmail.com>
2025-03-04 12:27:52 +0530

    dev server

--- src/pages/authentication/SignIn.jsx
+++ src/pages/authentication/SignIn.jsx
@@ -9,6 +9,7 @@ import { toast } from "react-toastify";
 
 const SignIn = () => {
   const dispatch = useDispatch();
+  console.log("dev server update");  
   const BASE_URL = import.meta.env.VITE_BASE_URL;
   const SIGNIN_URL = `${BASE_URL}/api/auth/signin`;
   const [formData, setFormData] = useState({
